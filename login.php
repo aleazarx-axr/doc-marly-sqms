e6fbf7953c32094386d6fed28b359e722c586981
Michael <justambot10@gmail.com>
2026-07-23 15:51:46 +0800

    added icon

--- login.php
+++ login.php
@@ -78,6 +78,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Doc Marly · Smart Queue Login</title>
+    <link rel="icon" type="image/png" href="assets/images/marly1.ico">
     <!-- Font Awesome 6 (free) for icons -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="assets/css/login_ui.css">
